b85a4eb5fd3ebeed72cae195e0b91f7fd45e4f7b
MohaElder <calen0909@hotmail.com>
2020-04-20 19:49:34 +0800

    updates to v 2.0

--- server.py
+++ server.py
@@ -14,32 +14,17 @@ class Config(object):
         'id': 'createschuler_job',
         'func': '__main__:timedClockIn',
         'trigger': 'interval',
-        'seconds': 86400,
+        'days': 1,
         'misfire_grace_time': 10000
     }]
-class person:
-    def __init__(self):
-        self.grade = ''
-        self.classNum = ''
-        self.studentID = ''
-        self.exerciseTime = '' 
-        self.email = '' 
-    def getstudentID(self):
-        return self.studentID
         
 app = Flask(__name__)
 CORS(app)
 execArr = []
+path = input("data file path: ")
 app.config.from_object(Config()) 
 scheduler = APScheduler()
 
-def cleanStr(word):
-    tempOne = word.replace("(","")
-    tempTwo = tempOne.replace("'","")
-    tempThree = tempTwo.replace(",","")
-    tempFour = tempThree.replace(")","")
-    print("New format:", tempFour)
-    return tempFour
 @app.route('/')
 def index():
     return 'It Works!'
@@ -49,7 +34,7 @@ def deleteStudent():
     print(request.form.get('id'))
     global execArr
     for i in range(0,len(execArr)):
-        if str(request.form.get('id')) == cleanStr(str(execArr[i].getstudentID())):
+        if str(request.form.get('id')) == execArr[i]['studentID']:
             print("found")
             del execArr[i]
             print("deleted")
@@ -60,35 +45,17 @@ def deleteStudent():
 
 @app.route('/clockin', methods=['POST'])
 def regClockIn():
-    global execArr
-    student = person()
-    student.grade = request.form.get('grade'),
-    student.classNum = request.form.get('class'),
-    student.studentID = request.form.get('id'),
-    student.exerciseTime = request.form.get('exerciseTime')
-    student.email = request.form.get('email')
-    execArr.append(student)
-    for student in execArr:
-        print(str(student.studentID) + str(student.grade) + str(student.classNum))
-    print(len(execArr), " Student(s) in execArr")
-    return "Registration Successful!"
-
+    if addStudent(request.form.get('grade'),request.form.get('class'),request.form.get('id'),request.form.get('exerciseTime'),request.form.get('email')):
+        return "Registration Successful!"
+    
 @app.route('/clockin', methods=['GET'])
 def getExecArr():
-    global execArr
-    students = []
-    for item in execArr:
-        students.append({
-            'Id': item.studentID,
-            'classNum': item.classNum,
-            'email': item.email
-        })
-    return json.dumps(students,ensure_ascii=False)
+    return json.dumps(execArr,ensure_ascii=False)
 
 @app.route('/mclockin', methods=['GET'])
 def manualClockin():
-    timedClockIn()
-    return "Manual Clockin Success"
+    if timedClockIn():
+        return "Manual Clockin Success"
     
 def sendEmail(email, imgUrl):
     if email == "":
@@ -100,7 +67,23 @@ def sendEmail(email, imgUrl):
                 'The attatched picture is your clock in status today. ']
     # 发送邮件
     yag.send(email, 'Auto Clock In Status', contents, [imgUrl])
-        
+
+def addStudent(grade,classNum,id,exerciseTime,email):
+    global execArr
+    student = {
+        "grade": grade,
+        "classNum": classNum,
+        "studentID": id,
+        "exerciseTime": exerciseTime,
+        "email": email
+        }
+    execArr.append(student)
+    saveData()
+    for student in execArr:
+        print(str(student['studentID']) + " " + str(student['grade']) + " " + str(student['classNum']))
+    print(len(execArr), " Student(s) in execArr")
+    return True
+
 def ClockIn(student):
     print(student)
     chrome_options = Options()
@@ -109,11 +92,11 @@ def ClockIn(student):
     start_url = "https://jinshuju.net/f/RrnsWl"
     driver.get(start_url)
     selector = Select(driver.find_element_by_id('entry_field_3_level_1'))  
-    selector.select_by_visible_text(student.grade)
+    selector.select_by_visible_text(student['grade'])
     selector = Select(driver.find_element_by_id('entry_field_3_level_2'))
-    selector.select_by_visible_text(student.classNum)
+    selector.select_by_visible_text(student['classNum'])
     inputter = driver.find_element_by_id('entry_field_31')
-    inputter.send_keys(student.studentID)
+    inputter.send_keys(student['studentID'])
     selector = Select(driver.find_element_by_id('entry_field_20'))
     selector.select_by_visible_text("Yes")
     selector = Select(driver.find_element_by_id('entry_field_32'))
@@ -123,25 +106,44 @@ def ClockIn(student):
     selector = Select(driver.find_element_by_id('entry_field_34'))
     selector.select_by_visible_text("Yes")
     inputter = driver.find_element_by_id('entry_field_37')
-    inputter.send_keys(student.exerciseTime)
+    inputter.send_keys(student['exerciseTime'])
     button = driver.find_element_by_name('commit')
     button.click()
-    path = 'C:\\Users\\Administrator\\Desktop\\'+ str(student.studentID) + '.png'
-    driver.save_screenshot(path)
-    sendEmail(str(student.email),path)
+    filePath = 'C:\\Users\\Administrator\\Desktop\\'+ str(student['studentID']) + '.png'
+    driver.save_screenshot(filePath)
+    sendEmail(str(student['email']),filePath)
     driver.quit()
     return 
-
+    
 def timedClockIn():
     print('TimeNow:%s' % (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
     if(len(execArr) == 0):
         print("no items yet")
-        return
+        return False
     for student in execArr:
         ClockIn(student)
     print("Clocked in ", len(execArr), " student(s)")
-    return
+    return True
+
+def fetchData():
+    global execArr
+    with open(path,"r") as f:
+        f.seek(0)
+        text = f.read()
+        if text != '': 
+            execArr = json.loads(text)
+            print(execArr)
+        else:
+            print("Emtpy file")
+        return
+
+def saveData():
+    f = open(path, 'w')
+    f.write(json.dumps(execArr))
+    f.close()
+    
 if __name__ == '__main__':
+    fetchData()
     scheduler=APScheduler()
     scheduler.init_app(app)
     scheduler.start()
